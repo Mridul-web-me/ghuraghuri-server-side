c0f01dd70af71288bf0e056dd9845d1d34abaa0b
Mridul-web-me <md.mridu.23@gmail.com>
2021-10-30 22:58:59 +0600

    database added

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 const { MongoClient } = require('mongodb');
+const cors = require('cors')
 require('dotenv').config()
 
 
 const port = process.env.PORT || 8080;
 
 
-
+// middleware
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kki34.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 console.log(uri);
@@ -18,6 +21,26 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         await client.connect();
+        const database = client.db('ghuraghuri');
+        const packagesCollection = database.collection('packages');
+
+
+        // GET API
+        app.get('/packages', async (req, res) => {
+            const cursor = packagesCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        })
+
+        // POST API
+        app.post('/packages', async (req, res) => {
+            const package = req.body;
+            console.log('hit the api', package);
+            const result = await packagesCollection.insertOne(package);
+            console.log(result);
+            res.json(result)
+        })
+
         console.log('Database Connected');
     }
     finally {
